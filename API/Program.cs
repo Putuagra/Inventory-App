49801c974c7c865e32abde90f08a269c1225526d
putuagra <iputuagra@gmail.com>
2023-09-19 14:01:51 +0700

    feat: add product, transaction, user service in program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -17,11 +17,17 @@ builder.Services.AddDbContext<InventoryDbContext>(options => options.UseSqlServe
 
 // Register repositories
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 // Register services
 builder.Services.AddScoped<CategoryServices>();
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<TransactionService>();
 builder.Services.AddScoped<SupplierService>();
+builder.Services.AddScoped<UserService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
