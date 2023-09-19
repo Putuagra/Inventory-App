25488acb5998f89a22c03644b2b8f35058c83232
ihsannurmaulana <81961193+ihsannurmaulana@users.noreply.github.com>
2023-09-19 10:29:09 +0700

    feat: create IGeneral Repository

---
+++ API/Contracts/IGeneralRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Contracts;
+
+public interface IGeneralRepository<TEntity>
+{
+    ICollection<TEntity> GetAll();
+    TEntity? Get(Guid guid);
+    TEntity? Create(TEntity entity);
+    bool Update(TEntity entity);
+    bool Delete(TEntity entity);
+}
