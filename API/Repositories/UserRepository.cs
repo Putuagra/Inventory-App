6adff95a574e6d66b099dd1b9531f33fa6373250
putuagra <iputuagra@gmail.com>
2023-09-19 10:44:38 +0700

    feat: create user repository

---
+++ API/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class UserRepository : GeneralRepository<User>, IUserRepository
+{
+    public UserRepository(InventoryDbContext context) : base(context)
+    {
+    }
+}
