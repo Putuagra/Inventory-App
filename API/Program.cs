422e7aca8ef24ec516e5f422e92c387eaf5ef4a9
putuagra <iputuagra@gmail.com>
2023-09-19 14:49:17 +0700

    feat: add fluent validation

--- API/Program.cs
+++ API/Program.cs
@@ -2,7 +2,10 @@ using API.Contracts;
 using API.Data;
 using API.Repositories;
 using API.Services;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +31,10 @@ builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<TransactionService>();
 builder.Services.AddScoped<SupplierService>();
 builder.Services.AddScoped<UserService>();
+
+// Register Fluent validation
+builder.Services.AddFluentValidationAutoValidation()
+    .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
