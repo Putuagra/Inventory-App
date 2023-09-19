8c400bb7236cf64c0877375919ab6618f072c319
putuagra <iputuagra@gmail.com>
2023-09-19 14:01:19 +0700

    feat: create user controller

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,5 +1,156 @@
-﻿namespace API.Controllers;
+﻿using API.DataTransferObjects.Users;
+using API.Services;
+using API.Utilities.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
-public class UserController
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
 {
+    private readonly UserService _userService;
+
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var users = _userService.Get();
+
+        if (!users.Any())
+        {
+            return NotFound(new ResponseHandler<UserDtoGet>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = "No users found",
+                Data = null
+            });
+        }
+
+        return Ok(new ResponseHandler<IEnumerable<UserDtoGet>>
+        {
+            Code = StatusCodes.Status200OK,
+            Status = HttpStatusCode.OK.ToString(),
+            Message = "users found",
+            Data = users
+        });
+    }
+
+    [HttpGet("{guid}")]
+    public IActionResult Get(Guid guid)
+    {
+        var user = _userService.Get(guid);
+        if(user is null)
+        {
+            return NotFound(new ResponseHandler<UserDtoGet>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = "No user found",
+                Data = null
+            });
+        }
+        return Ok(new ResponseHandler<UserDtoGet>
+        {
+            Code = StatusCodes.Status200OK,
+            Status = HttpStatusCode.OK.ToString(),
+            Message = "User found",
+            Data = user
+        });
+    }
+
+    [HttpPost]
+    public IActionResult Create(UserDtoCreate userDtoCreate)
+    {
+        var userCreated = _userService.Create(userDtoCreate);
+        if(userCreated is null)
+        {
+            return BadRequest(new ResponseHandler<UserDtoCreate>
+            {
+                Code = StatusCodes.Status400BadRequest,
+                Status = HttpStatusCode.BadRequest.ToString(),
+                Message = "User not created",
+                Data = null
+            });
+        }
+        return Ok(new ResponseHandler<UserDtoCreate>
+        {
+            Code = StatusCodes.Status201Created,
+            Status = HttpStatusCode.Created.ToString(),
+            Message = "User created",
+            Data = userCreated
+        });
+    }
+
+    [HttpPut]
+    public IActionResult Update(UserDtoUpdate userDtoUpdate)
+    {
+        var userUpdated = _userService.Update(userDtoUpdate);
+
+        if(userUpdated == -1)
+        {
+            return NotFound(new ResponseHandler<UserDtoUpdate>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = "User not found",
+                Data = null
+            });
+        }
+        if(userUpdated == 0)
+        {
+            return BadRequest(new ResponseHandler<UserDtoUpdate>
+            {
+                Code = StatusCodes.Status400BadRequest,
+                Status = HttpStatusCode.BadRequest.ToString(),
+                Message = "User not updated",
+                Data = null
+            });
+        }
+        return Ok(new ResponseHandler<UserDtoUpdate>
+        {
+            Code = StatusCodes.Status200OK,
+            Status = HttpStatusCode.OK.ToString(),
+            Message = "User updated",
+            Data = userDtoUpdate
+        });
+    }
+
+    [HttpDelete("{guid}")]
+    public IActionResult Delete(Guid guid)
+    {
+        var userDeleted = _userService.Delete(guid);
+
+        if(userDeleted == -1) {
+            return NotFound(new ResponseHandler<UserDtoGet>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = "User not found",
+                Data = null
+            });
+        }
+        if(userDeleted == 0)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHandler<UserDtoGet>
+            {
+                Code = StatusCodes.Status500InternalServerError,
+                Status = HttpStatusCode.InternalServerError.ToString(),
+                Message = "User not deleted",
+                Data = null
+            });
+        }
+        return Ok(new ResponseHandler<UserDtoGet>
+        {
+            Code = StatusCodes.Status200OK,
+            Status = HttpStatusCode.OK.ToString(),
+            Message = "User deleted",
+        });
+    }
 }
