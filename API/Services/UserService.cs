bc43d7ad26da42dc7c35ddf5eeefaeed63a8b07d
putuagra <iputuagra@gmail.com>
2023-09-19 14:48:25 +0700

    feat: create user register service

--- API/Services/UserService.cs
+++ API/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using API.Contracts;
+using API.Data;
 using API.DataTransferObjects.Users;
 using API.Models;
 
@@ -6,11 +7,13 @@ namespace API.Services;
 
 public class UserService
 {
+    private readonly InventoryDbContext _inventoryDbContext;
     private readonly IUserRepository _userRepository;
 
-    public UserService(IUserRepository userRepository)
+    public UserService(IUserRepository userRepository, InventoryDbContext inventoryDbContext)
     {
         _userRepository = userRepository;
+        _inventoryDbContext = inventoryDbContext;
     }
 
     public IEnumerable<UserDtoGet> Get()
@@ -55,5 +58,21 @@ public class UserService
         if (user is null) return -1;
         var userDeleted = _userRepository.Delete(user);
         return userDeleted ? 1 : 0;
+    }
+
+    public bool Register(UserDtoRegister userDtoRegister)
+    {
+        using var transaction = _inventoryDbContext.Database.BeginTransaction();
+
+        try
+        {
+            _userRepository.Create(userDtoRegister);
+            transaction.Commit();
+            return true;
+        }catch 
+        {
+            transaction.Rollback();
+            return false;
+        }
     }
 }
