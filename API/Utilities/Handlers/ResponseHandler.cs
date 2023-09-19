459cfcf1becb85d75897ece9903356cc2c10feef
ihsannurmaulana <81961193+ihsannurmaulana@users.noreply.github.com>
2023-09-19 11:50:42 +0700

    feat: update response handler

--- API/Utilities/Handlers/ResponseHandler.cs
+++ API/Utilities/Handlers/ResponseHandler.cs
@@ -5,5 +5,5 @@ public class ResponseHandler<TEntity>
     public int Code { get; set; }
     public string Status { get; set; }
     public string Message { get; set; } 
-    public TEntity? Date { get; set; }
+    public TEntity? Data { get; set; }
 }
