3d45cc17c7eb61ec1937ff5a401a0626e11faadd
putuagra <iputuagra@gmail.com>
2023-09-19 10:44:17 +0700

    feat: create supplier repository

---
+++ API/Repositories/SupplierRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class SupplierRepository : GeneralRepository<Supplier>, ISupplierRepository
+{
+    public SupplierRepository(InventoryDbContext context) : base(context)
+    {
+    }
+}
