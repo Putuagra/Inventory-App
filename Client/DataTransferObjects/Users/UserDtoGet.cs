9d372c159962898050de829172c1b05ce9715cd5
putuagra <iputuagra@gmail.com>
2023-09-20 16:52:01 +0700

    feat: add user dto get in client

---
+++ Client/DataTransferObjects/Users/UserDtoGet.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.DataTransferObjects.Users;
+
+public class UserDtoGet
+{
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
