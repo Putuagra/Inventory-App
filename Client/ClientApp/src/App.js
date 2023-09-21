470e98036d3c5cdee796db336ca70bf994e43fb8
putuagra <iputuagra@gmail.com>
2023-09-21 16:22:25 +0700

    refactor: change app.js

--- Client/ClientApp/src/App.js
+++ Client/ClientApp/src/App.js
@@ -1,49 +1,10 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import UserList from "./components/UserList";
 
 const App = () => {
 
-    const [users, setUsers] = useState({ users: [] });
-
-    useEffect(() => {
-        axios.get("https://localhost:7020/api/User")
-            .then(response => {
-                setUsers(response.data.data); // Perubahan ini
-            })
-            .catch(error => {
-                console.error("Error fetching data:", error);
-            });
-    }, []);
-
     return (
         <div className="container">
-            <h1>Users</h1>
-            <div className="row">
-                <div className="col-sm-12">
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {Array.isArray(users) ? (
-                                users.map((data, index) => (
-                                    <tr key={index}>
-                                        <td>{data.name}</td>
-                                        <td>{data.email}</td>
-                                    </tr>
-                                ))
-                            ) : (
-                                <tr>
-                                    <td colSpan="2">No user data available.</td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+            <UserList />
         </div>
     )
 }
