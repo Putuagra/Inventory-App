fcfa89aa77f5759df8b7791086fbb9e474b9bb47
putuagra <iputuagra@gmail.com>
2023-09-21 16:22:41 +0700

    feat: add api user in client

---
+++ Client/ClientApp/src/apis/UserAPI.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const apiUrl = 'https://localhost:7020/api';
+
+export const getAll = async () => {
+    try {
+        const response = await axios.get(`${apiUrl}/User`);
+        return response.data.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const create = async (userData) => {
+    try {
+        const response = await axios.post(`${apiUrl}/User`, userData);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const update = async (updatedData) => {
+    try {
+        const response = await axios.put(`${apiUrl}/User`, updatedData);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const remove = async (userId) => {
+    try {
+        const response = await axios.delete(`${apiUrl}/User/${userId}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
