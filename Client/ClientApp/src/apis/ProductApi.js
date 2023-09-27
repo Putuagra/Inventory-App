b0473bafbf718f8665396f07f9aa1c4c1e96563f
putuagra <iputuagra@gmail.com>
2023-09-27 10:07:09 +0700

    feat: add product api in client

---
+++ Client/ClientApp/src/apis/ProductApi.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const apiUrl = 'https://localhost:7020/api';
+
+export const getAllProducts = async () => {
+    try {
+        const response = await axios.get(`${apiUrl}/Product`);
+        return response.data.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const create = async (userData) => {
+    try {
+        const response = await axios.post(`${apiUrl}/Product`, userData);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const update = async (updatedData) => {
+    try {
+        const response = await axios.put(`${apiUrl}/Product`, updatedData);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const remove = async (productGuid) => {
+    try {
+        const response = await axios.delete(`${apiUrl}/Product/${productGuid}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
