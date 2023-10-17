b16781d61ba97f84f30541e6aa9721a2f0de3917
putuagra <iputuagra@gmail.com>
2023-10-17 10:36:16 +0700

    feat: add select component

---
+++ Client/ClientApp/src/components/Select.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Select = ({ name, label, value, onChange, options }) => {
+    return (
+        <div className="col-md-6">
+            <label className="form-label">{label}</label>
+            <select
+                className="form-input"
+                name={name}
+                value={value}
+                onChange={onChange}
+                required
+            >
+                <option value="">Select {label}</option>
+                {options.map((option) => (
+                    <option key={option.guid} value={option.guid}>
+                        {option.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+        
+    );
+};
+
+export default Select;
