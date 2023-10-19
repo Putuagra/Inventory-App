fc7f4da40d92dbd9542a780c86e29c329a26deaa
putuagra <iputuagra@gmail.com>
2023-10-19 16:14:48 +0700

    feat: add error handling supplier form

--- Client/ClientApp/src/components/suppliers/SupplierForm.js
+++ Client/ClientApp/src/components/suppliers/SupplierForm.js
@@ -1,24 +1,75 @@
 import React, { useState } from 'react';
 import Input from '../Input';
 import Button from '../Button';
+import axios from "axios";
+import toast, { Toaster } from 'react-hot-toast';
+import SupplierValidation from '../../Validation/SupplierValidation';
 
 export default function SupplierForm({ handleCreate }) {
     const [newSupplier, setNewSupplier] = useState({ name: '', address: '', email: '', phoneNumber: '' });
+
+    const [errors, setErrors] = useState({})
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setNewSupplier({ ...newSupplier, [name]: value });
     };
 
+    const handleCreateSupplier = async () => {
+        const namePattern = /^[a-zA-Z0-9]+$/
+        const addressPattern = /^[a-zA-Z0-9\s]+$/
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+        const phoneNumberPattern = /^\+[1-9]\d{1,20}$/
+        const apiUrl = 'https://localhost:7020/api'
+        if (!newSupplier.name || !newSupplier.email || !newSupplier.address || !newSupplier.phoneNumber) {
+            return;
+        }
+
+        if (!namePattern.test(newSupplier.name) ||
+            !emailPattern.test(newSupplier.email) ||
+            !addressPattern.test(newSupplier.address) ||
+            !phoneNumberPattern.test(newSupplier.phoneNumber)
+        ) {
+            return;
+        }
+
+        try {
+            const responseEmail = await axios.get(`${apiUrl}/Supplier/ByEmail/${newSupplier.email}`);
+
+            if (responseEmail.status === 200) {
+                alert('Email already exists. Please use a different email.');
+                return;
+            }
+        } catch {
+            console.log('Something went wrong with the email response.');
+        }
+
+        try {
+            const responsePhone = await axios.get(`${apiUrl}/Supplier/ByPhone/${newSupplier.phoneNumber}`);
+
+            if (responsePhone.status === 200) {
+                alert('Phone number already exists.');
+                return;
+            }
+        } catch {
+            console.log('Something went wrong with the phone response.');
+        }
+       
+        handleCreate(newSupplier);
+        setNewSupplier({ name: '', address: '', email: '', phoneNumber: '' });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setErrors(SupplierValidation(newSupplier))
+        handleCreateSupplier()
+    }
+
     return (
         <div className="row">
             <div className="col-lg-12" noValidate>
                 <form
-                    onSubmit={(e) => {
-                        e.preventDefault()
-                        handleCreate(newSupplier);
-                        setNewSupplier({ name: '', address: '', email: '', phoneNumber: '' });
-                    }}
+                    onSubmit={handleSubmit}
                     className="row g-3 needs-validation"
                 >
                     <Input
@@ -27,6 +78,7 @@ export default function SupplierForm({ handleCreate }) {
                         placeholder="Supplier Name"
                         value={newSupplier.name}
                         onChange={handleChange}
+                        errors={errors.name}
                     />
                     <Input
                         name="address"
@@ -34,6 +86,7 @@ export default function SupplierForm({ handleCreate }) {
                         placeholder="Address"
                         value={newSupplier.address}
                         onChange={handleChange}
+                        errors={errors.address}
                     />
                     <Input
                         name="email"
@@ -41,6 +94,7 @@ export default function SupplierForm({ handleCreate }) {
                         placeholder="Email"
                         value={newSupplier.email}
                         onChange={handleChange}
+                        errors={errors.email}
                     />
                     <Input
                         name="phoneNumber"
@@ -48,6 +102,7 @@ export default function SupplierForm({ handleCreate }) {
                         placeholder="Phone Number"
                         value={newSupplier.phoneNumber}
                         onChange={handleChange}
+                        errors={errors.phoneNumber}
                     />
                     <Button
                         name="Add Supplier"
