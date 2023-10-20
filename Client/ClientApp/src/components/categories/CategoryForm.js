6ba51e03ffc230222e2bd54b35cbdbc0a55d6b7f
putuagra <iputuagra@gmail.com>
2023-10-20 10:14:53 +0700

    feat: add error handling category form

--- Client/ClientApp/src/components/categories/CategoryForm.js
+++ Client/ClientApp/src/components/categories/CategoryForm.js
@@ -2,24 +2,56 @@ import React, { useState } from 'react';
 import Button from '../Button';
 import Input from '../Input';
 import Select from '../Select';
+import axios from "axios";
+import CategoryValidation from '../../Validation/CategoryValidation';
 
 export default function CategoryForm({ handleCreate,suppliers }) {
     const [newCategory, setNewCategory] = useState({ name: '', supplierGuid: '' });
+    const [errors, setErrors] = useState({})
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setNewCategory({ ...newCategory, [name]: value });
     };
 
+    const handleCreateCategory = async () => {
+        const namePattern = /^[a-zA-Z0-9\s]+$/
+        const apiUrl = 'https://localhost:7020/api'
+
+        if (!newCategory.name || !newCategory.supplierGuid) {
+            return;
+        }
+
+        if (!namePattern.test(newCategory.name)) {
+            return;
+        }
+
+        try {
+            const response = await axios.get(`${apiUrl}/Category/CheckDuplicate/${newCategory.name}/${newCategory.supplierGuid}`);
+
+            if (response.status === 200) {
+                alert('Category Name already exists.');
+                return;
+            }
+        } catch {
+            console.log('Something went wrong with the category response.');
+        }
+
+        handleCreate(newCategory);
+        setNewCategory({ name: '', supplierGuid: '' });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setErrors(CategoryValidation(newCategory))
+        handleCreateCategory()
+    }
+
     return (
         <div className="row">
             <div className="col-lg-12" noValidate>
                 <form
-                    onSubmit={(e) => {
-                        e.preventDefault()
-                        handleCreate(newCategory);
-                        setNewCategory({ name: '', supplierGuid: '' });
-                    }}
+                    onSubmit={handleSubmit}
                     className="row g-3 needs-validation"
                 >
                     <Input
@@ -28,6 +60,7 @@ export default function CategoryForm({ handleCreate,suppliers }) {
                         placeholder="Category Name"
                         value={newCategory.name}
                         onChange={handleChange}
+                        errors={errors.name}
                     />
                     <Select
                         name="supplierGuid"
