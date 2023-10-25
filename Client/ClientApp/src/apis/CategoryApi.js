9ab07ea02663277e2ea88370654c28216b947194
putuagra <iputuagra@gmail.com>
2023-10-25 16:12:57 +0700

    feat: add check availability and duplicate api in client

--- Client/ClientApp/src/apis/CategoryApi.js
+++ Client/ClientApp/src/apis/CategoryApi.js
@@ -1,43 +1,67 @@
-import axios from "axios";
+import axios from "axios"
 
-const apiUrl = 'https://localhost:7020/api';
+const apiUrl = 'https://localhost:7020/api'
 
 export const getAll = async () => {
     try {
-        const response = await axios.get(`${apiUrl}/Category`);
-        return response.data.data;
+        const response = await axios.get(`${apiUrl}/Category`)
+        return response.data.data
     } catch (error) {
         console.log(error);
-        throw error;
+        throw error
     }
 };
 
 export const create = async (userData) => {
     try {
-        const response = await axios.post(`${apiUrl}/Category`, userData);
-        return response.data;
+        const response = await axios.post(`${apiUrl}/Category`, userData)
+        return response.data
     } catch (error) {
-        console.log(error);
-        throw error;
+        console.log(error)
+        throw error
     }
 };
 
 export const update = async (updatedData) => {
     try {
-        const response = await axios.put(`${apiUrl}/Category`, updatedData);
-        return response.data;
+        const response = await axios.put(`${apiUrl}/Category`, updatedData)
+        return response.data
     } catch (error) {
-        console.log(error);
-        throw error;
+        console.log(error)
+        throw error
     }
 };
 
 export const remove = async (supllierGuid) => {
     try {
-        const response = await axios.delete(`${apiUrl}/Category/${supllierGuid}`);
-        return response.data;
+        const response = await axios.delete(`${apiUrl}/Category/${supllierGuid}`)
+        return response.data
     } catch (error) {
-        console.log(error);
-        throw error;
+        console.log(error)
+        throw error
+    }
+};
+
+export const checkAvailability = async (categoryGuid, supplierGuid) => {
+    try {
+        const response = await axios.get(`${apiUrl}/Category/CheckAvailability/${categoryGuid}/${supplierGuid}`)
+        return response.status
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            return 404
+        }
+        console.error('Error during API request:', error)
+    }
+};
+
+export const checkDuplicate = async (name, supplierGuid) => {
+    try {
+        const response = await axios.get(`${apiUrl}/Category/CheckDuplicate/${name}/${supplierGuid}`)
+        return response.status
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            return 404
+        }
+        console.error('Error during API request:', error)
     }
 };
