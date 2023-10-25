6ade2846387d4c5a68050260af86ca89098483cd
putuagra <iputuagra@gmail.com>
2023-10-25 16:13:24 +0700

    feat: add check product availability api in client

--- Client/ClientApp/src/apis/ProductApi.js
+++ Client/ClientApp/src/apis/ProductApi.js
@@ -1,53 +1,65 @@
-import axios from "axios";
+import axios from "axios"
 
-const apiUrl = 'https://localhost:7020/api';
+const apiUrl = 'https://localhost:7020/api'
 
 export const getAllProducts = async () => {
     try {
-        const response = await axios.get(`${apiUrl}/Product`);
-        return response.data.data;
+        const response = await axios.get(`${apiUrl}/Product`)
+        return response.data.data
     } catch (error) {
-        console.log(error);
-        throw error;
+        console.log(error)
+        throw error
     }
 }
 
 export const create = async (userData) => {
     try {
-        const response = await axios.post(`${apiUrl}/Product`, userData);
-        return response.data;
+        const response = await axios.post(`${apiUrl}/Product`, userData)
+        return response.data
     } catch (error) {
-        console.log(error);
-        throw error;
+        console.log(error)
+        throw error
     }
 }
 
 export const update = async (updatedData) => {
     try {
-        const response = await axios.put(`${apiUrl}/Product`, updatedData);
-        return response.data;
+        const response = await axios.put(`${apiUrl}/Product`, updatedData)
+        return response.data
     } catch (error) {
-        console.log(error);
-        throw error;
+        console.log(error)
+        throw error
     }
 }
 
 export const remove = async (productGuid) => {
     try {
-        const response = await axios.delete(`${apiUrl}/Product/${productGuid}`);
-        return response.data;
+        const response = await axios.delete(`${apiUrl}/Product/${productGuid}`)
+        return response.data
     } catch (error) {
-        console.log(error);
-        throw error;
+        console.log(error)
+        throw error
     }
 }
 
 export const updateStock = async (updatedStock) => {
     try {
         const response = await axios.put(`${apiUrl}/Product`, updatedStock)
-        return response.data;
+        return response.data
     } catch (error) {
-        console.error('Error updating stock:', error);
-        throw error;
+        console.error('Error updating stock:', error)
+        throw error
     }
 }
+
+export const checkProductAvailability = async (name, supplierGuid, categoryGuid) => {
+    try {
+        const response = await axios.get(`${apiUrl}/Product/CheckDuplicate/${name}/${supplierGuid}/${categoryGuid}`)
+        return response.status
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            return 404
+        }
+        console.error('Error during API request:', error)
+    }
+};
