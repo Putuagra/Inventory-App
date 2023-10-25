dac335f89c13356ff58e005a56118c99319b3efd
putuagra <iputuagra@gmail.com>
2023-10-25 16:14:15 +0700

    feat: add input handle category list

--- Client/ClientApp/src/components/categories/CategoryList.js
+++ Client/ClientApp/src/components/categories/CategoryList.js
@@ -1,6 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react'
+import Button from '../Button'
+import InputUpdate from '../Input'
+import Select from '../Select'
+import SuccessAlert from '../SuccessAlert'
+import ErrorAlert from '../ErrorAlert'
 
-export default function CategoryList({ categories, suppliers, editingCategory, handleEdit, handleInputChange, handleUpdate, handleDelete }) {
+export default function CategoryList({ categories, suppliers, editingCategory, handleEdit, handleInputChange, handleUpdate, handleDelete, handleDuplicate }) {
+
+    const [category, setCategory] = useState('')
+
+    const handleCategoryEdit = (guid) => {
+        const categoryToEdit = categories.find((category) => category.guid === guid)
+        setCategory(categoryToEdit.name)
+    }
+
+    const handleUpdateCategory = async (data) => {
+        const namePattern = /^[a-zA-Z0-9\s]+$/
+
+        if (data.name === '' && data.supplierGuid === '') {
+            ErrorAlert({ message: 'Semua field harus diisi.' })
+            return
+        }
+
+        if (data.name === '') {
+            ErrorAlert({ message: 'Nama harus diisi.' })
+            return
+        } else if (!namePattern.test(data.name)) {
+            ErrorAlert({ message: 'Invalid format name.' })
+            return
+        }
+
+        if (data.supplierGuid === '') {
+            ErrorAlert({ message: 'Supplier harus diisi.' })
+            return
+        }
+
+        const status = await handleDuplicate(data.name, data.supplierGuid)
+
+        if (status === 200 && category !== data.name) {
+            ErrorAlert({ message: 'Category name already exists in this supplier.' })
+            return
+        }
+
+        if (status === 404  || category === data.name) {
+            try {
+                await handleUpdate(data)
+                SuccessAlert({ message: 'Update category successful.' })
+            } catch (error) {
+                console.error('Error during update:', error)
+                ErrorAlert({ message: 'Failed to update category. Please try again later.' })
+            }
+        } 
+    }
+
     return (
         <table className="table table-striped">
             <thead>
@@ -16,7 +68,8 @@ export default function CategoryList({ categories, suppliers, editingCategory, h
                         <tr key={index}>
                             <td>
                                 {editingCategory === data.guid ? (
-                                    <input
+                                    <InputUpdate
+                                        name="name"
                                         type="text"
                                         value={data.name}
                                         onChange={(e) => handleInputChange(data.guid, 'name', e.target.value)}
@@ -27,46 +80,39 @@ export default function CategoryList({ categories, suppliers, editingCategory, h
                             </td>
                             <td>
                                 {editingCategory === data.guid ? (
-                                    <select
+                                    <Select
+                                        name="supplierGuid"
+                                        label="Supplier"
                                         value={data.supplierGuid || ''}
                                         onChange={(e) => handleInputChange(data.guid, 'supplierGuid', e.target.value)}
-                                    >
-                                        <option value="">Select Supplier</option>
-                                        {suppliers.map((supplier) => (
-                                            <option key={supplier.guid} value={supplier.guid}>
-                                                {supplier.name}
-                                            </option>
-                                        ))}
-                                    </select>
+                                        options={suppliers}
+                                    />
                                 ) : (
                                     (suppliers.find((supplier) => supplier.guid === data.supplierGuid) || {}).name
                                 )}
                             </td>
                             <td>
                                 {editingCategory === data.guid ? (
-                                    <button
-                                        type="button"
+                                    <Button
+                                        name="Save"
                                         className="btn btn-success"
-                                        onClick={() => handleUpdate(data)}
-                                    >
-                                        Save
-                                    </button>
+                                        onClick={() => handleUpdateCategory(data)}
+                                    />
                                 ) : (
-                                    <>
-                                        <button
-                                            type="button"
-                                            className="btn btn-primary"
-                                            onClick={() => handleEdit(data.guid)}
-                                        >
-                                            Edit
-                                        </button>
-                                        <button
-                                            type="button"
-                                            className="btn btn-danger"
-                                            onClick={() => handleDelete(data.guid)}
-                                        >
-                                            Delete
-                                        </button>
+                                        <>
+                                            <Button
+                                                name="Edit"
+                                                className="btn btn-primary"
+                                                onClick={() => {
+                                                    handleCategoryEdit(data.guid)
+                                                    handleEdit(data.guid)
+                                                }}
+                                            />
+                                            <Button
+                                                name="Delete"
+                                                className="btn btn-danger"
+                                                onClick={() => handleDelete(data.guid)}
+                                            />
                                     </>
                                 )}
                             </td>
