f416f72932cf3736b509e34e7f06727a83d87f6d
putuagra <iputuagra@gmail.com>
2023-10-30 10:52:32 +0700

    feat: add delete alert client

---
+++ Client/ClientApp/src/components/DeleteAlert.jsx
@@ -0,0 +1,24 @@
+import Swal from 'sweetalert2'
+
+const DeleteAlert = async ({ handleDelete, guid }) => {
+    const result = await Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    })
+
+    if (result.isConfirmed) {
+        await handleDelete(guid)
+        Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+        )
+    }
+}
+
+export default DeleteAlert
