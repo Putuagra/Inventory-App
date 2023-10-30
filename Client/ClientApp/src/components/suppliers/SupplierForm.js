a141974e12489288b8ae24ca861e0af73eadf467
putuagra <iputuagra@gmail.com>
2023-10-30 10:57:26 +0700

    feat: add name validation supplier form

--- Client/ClientApp/src/components/suppliers/SupplierForm.js
+++ Client/ClientApp/src/components/suppliers/SupplierForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import Input from '../Input'
 import Button from '../Button'
-import SupplierValidation from '../../Validation/SupplierValidation'
+import SupplierValidation from '../../Validation/Suppliers/SupplierValidation'
 import SuccessAlert from '../SuccessAlert'
 import ErrorAlert from '../ErrorAlert'
+import ValidateData from '../../Validation/Suppliers/SupplierPattern'
 
-export default function SupplierForm({ handleCreate, handleEmail, handlePhoneNumber }) {
+export default function SupplierForm({ handleCreate, handleEmail, handlePhoneNumber, handleName }) {
     const [newSupplier, setNewSupplier] = useState({ name: '', address: '', email: '', phoneNumber: '' })
 
     const [errors, setErrors] = useState({})
@@ -16,29 +17,26 @@ export default function SupplierForm({ handleCreate, handleEmail, handlePhoneNum
     }
 
     const handleCreateSupplier = async () => {
-        const namePattern = /^[a-zA-Z0-9]+$/
-        const addressPattern = /^[a-zA-Z0-9\s]+$/
-        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
-        const phoneNumberPattern = /^\+[1-9]\d{1,20}$/
-        if (!newSupplier.name || !newSupplier.email || !newSupplier.address || !newSupplier.phoneNumber) {
+        const validationError = ValidateData(newSupplier)
+
+        if (validationError) {
+            ErrorAlert({ message: validationError })
             return
         }
 
-        if (!namePattern.test(newSupplier.name) ||
-            !emailPattern.test(newSupplier.email) ||
-            !addressPattern.test(newSupplier.address) ||
-            !phoneNumberPattern.test(newSupplier.phoneNumber)
-        ) {
+        const nameStatus = await handleName(newSupplier.name)
+        if (nameStatus === 200) {
+            ErrorAlert({ message: 'Name supplier already exists. Please use a different name.' })
             return
         }
 
-        const emailStatus = await handleEmail(newSupplier.email);
+        const emailStatus = await handleEmail(newSupplier.email)
         if (emailStatus === 200) {
             ErrorAlert({ message: 'Email already exists. Please use a different email.' })
             return
         }
 
-        const phoneStatus = await handlePhoneNumber(newSupplier.phoneNumber);
+        const phoneStatus = await handlePhoneNumber(newSupplier.phoneNumber)
         if (phoneStatus === 200) {
             ErrorAlert({ message: 'Phone number already exists. Please use a different number.' })
             return
