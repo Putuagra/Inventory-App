8d4fd8db060294628628dd079b061386c6ffe4e7
putuagra <iputuagra@gmail.com>
2023-10-30 11:08:48 +0700

    feat: refactor validation

--- Client/ClientApp/src/Validation/Suppliers/SupplierValidation.js
+++ Client/ClientApp/src/Validation/Suppliers/SupplierValidation.js
@@ -1,10 +1,10 @@
-export default function SupplierValidation(value) {
-    const errors = {}
+const namePattern = /^[a-zA-Z0-9]+$/
+const addressPattern = /^[a-zA-Z0-9\s]+$/
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+const phoneNumberPattern = /^\+[1-9]\d{1,20}$/
 
-    const namePattern = /^[a-zA-Z0-9]+$/
-    const addressPattern = /^[a-zA-Z0-9\s]+$/
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
-    const phoneNumberPattern = /^\+[1-9]\d{1,20}$/
+export function SupplierValidation(value) {
+    const errors = {}
 
     if (value.name === "") {
         errors.name = "Name is required."
@@ -31,4 +31,53 @@ export default function SupplierValidation(value) {
     }
 
     return errors
+}
+
+export function ValidateData(data){
+    if (data.name === '') {
+        return 'Nama harus diisi.'
+    } else if (!namePattern.test(data.name)) {
+        return 'Invalid format name.'
+    }
+
+    if (data.email === '') {
+        return 'Email harus diisi.'
+    } else if (!emailPattern.test(data.email)) {
+        return 'Invalid format email.'
+    }
+
+    if (data.address === '') {
+        return 'Address harus diisi.'
+    } else if (!addressPattern.test(data.address)) {
+        return 'Invalid format address.'
+    }
+
+    if (data.phoneNumber === '') {
+        return 'Phone number harus diisi.'
+    } else if (!phoneNumberPattern.test(data.phoneNumber)) {
+        return 'Invalid format phone number.'
+    }
+
+    return null
+}
+
+export function ValidationDuplicate(data, name, email, phoneNumber, emailStatus, phoneStatus, nameStatus){
+
+    if (nameStatus === 200 && name !== data.name) {
+        return 'Name supplier already exists. Please use a different name.'
+    } else if (nameStatus === 500 || nameStatus === 400) {
+        return 'Failed to check name supplier availability. Please try again later.'
+    }
+
+    if (emailStatus === 200 && email !== data.email) {
+        return 'Email already exists. Please use a different email.'
+    } else if (emailStatus === 500 || emailStatus === 400) {
+        return 'Failed to check email availability. Please try again later.'
+    }
+
+    if (phoneStatus === 200 && phoneNumber !== data.phoneNumber) {
+        return 'Phone number already exists. Please use a different number.'
+    } else if (phoneStatus === 500 || phoneStatus === 400) {
+        return 'Failed to check phone number availability. Please try again later.'
+    }
 }
