d8651dd7613f93366d0f23d529b0fc177064f9af
putuagra <iputuagra@gmail.com>
2023-10-31 13:35:42 +0700

    feat: add router in app

--- Client/ClientApp/src/App.js
+++ Client/ClientApp/src/App.js
@@ -1,20 +1,26 @@
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import CategoryRepository from "./repositories/CategoryRepository";
 import ProductRepository from "./repositories/ProductRepository";
 import UserRepository from "./repositories/UserRepository"
 import TransactionRepository from "./repositories/TransactionRepository";
 import SupplierRepository from "./repositories/SupplierRepository"
+import "./custom.css"
 
 const App = () => {
 
     return (
         <div className="container">
-            {/*<SupplierRepository />
-            <CategoryRepository />*/}
-            <ProductRepository />
-            {/*<UserRepository />*/}
-            <TransactionRepository />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<ProductRepository /> } />
+                    <Route path="/supplier" element={<SupplierRepository /> } />
+                    <Route path="/category" element={<CategoryRepository /> } />
+                    <Route path="/user" element={<UserRepository /> } />
+                    <Route path="/transaction" element={<TransactionRepository /> } />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
 
-export default App;
+export default App
