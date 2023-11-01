5dd5ae881150a33851c6bec09bcb6bb222f93b54
putuagra <iputuagra@gmail.com>
2023-11-01 14:26:29 +0700

    feat: add route path app

--- Client/ClientApp/src/App.js
+++ Client/ClientApp/src/App.js
@@ -1,10 +1,16 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
-import CategoryRepository from "./repositories/CategoryRepository";
-import ProductRepository from "./repositories/ProductRepository";
-import UserRepository from "./repositories/UserRepository"
-import TransactionRepository from "./repositories/TransactionRepository";
-import SupplierRepository from "./repositories/SupplierRepository"
 import "./custom.css"
+import Register from './pages/Register'
+import CategoryPage from './pages/CategoryPage'
+import ProductPage from './pages/ProductPage'
+import UserPage from './pages/UserPage'
+import SupplierPage from './pages/SupplierPage'
+import TransactionPage from './pages/TransactionPage'
+import AddCategory from './pages/AddCategory'
+import AddProduct from './pages/AddProduct'
+import AddSupplier from './pages/AddSupplier'
+import AddTransaction from './pages/AddTransaction'
+
 
 const App = () => {
 
@@ -12,11 +18,16 @@ const App = () => {
         <div className="container">
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<ProductRepository /> } />
-                    <Route path="/supplier" element={<SupplierRepository /> } />
-                    <Route path="/category" element={<CategoryRepository /> } />
-                    <Route path="/user" element={<UserRepository /> } />
-                    <Route path="/transaction" element={<TransactionRepository /> } />
+                    <Route path="/" element={<ProductPage />} />
+                    <Route path="/supplier" element={<SupplierPage />} />
+                    <Route path="/category" element={<CategoryPage />} />
+                    <Route path="/user" element={<UserPage />} />
+                    <Route path="/transaction" element={<TransactionPage />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/add-supplier" element={<AddSupplier />} />
+                    <Route path="/add-category" element={<AddCategory />} />
+                    <Route path="/add-product" element={<AddProduct />} />
+                    <Route path="/add-transaction" element={<AddTransaction />} />
                 </Routes>
             </BrowserRouter>
         </div>
