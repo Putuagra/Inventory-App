08959c6aa81d81d9fde590f5aa71af89a81e5447
putuagra <iputuagra@gmail.com>
2023-11-01 14:28:33 +0700

    feat: refactor page product

---
+++ Client/ClientApp/src/pages/AddProduct.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from 'react-router-dom'
+import ProductRepository from "../repositories/ProductRepository"
+import Button from "../components/Button"
+import ProductForm from '../components/products/ProductForm'
+import { checkProductAvailability } from '../apis/ProductApi'
+
+
+const AddCategory = () => {
+    const navigate = useNavigate()
+    const handleProductClick = () => {
+        navigate("/")
+    }
+    const { handleCreate, suppliers, categories } = ProductRepository()
+    return (
+        <div>
+            <ProductForm
+                handleCreate={handleCreate}
+                suppliers={suppliers}
+                categories={categories}
+                handleCheckProduct={checkProductAvailability}
+            />
+            <Button
+                name="Back"
+                className="btn btn-primary"
+                onClick={handleProductClick}
+            />
+        </div>
+    )
+}
+
+export default AddCategory
---
+++ Client/ClientApp/src/pages/ProductPage.jsx
@@ -0,0 +1,40 @@
+import { checkAvailability } from "../apis/CategoryApi"
+import { checkProductAvailability } from "../apis/ProductApi"
+import { useNavigate } from 'react-router-dom'
+import ProductRepository from "../repositories/ProductRepository"
+import Button from "../components/Button"
+import Navigate from "../components/Navigate"
+import ProductList from "../components/products/ProductList"
+
+const ProductPage = () => {
+    const navigate = useNavigate()
+    const { products, categories, suppliers, editingProduct, handleEdit, handleInputChange, handleUpdate, handleDelete } = ProductRepository()
+    const handleAddProductClick = () => {
+        navigate("/add-product")
+    }
+    return (
+        <div className="container">
+            <Navigate />
+            <h1>Products</h1>
+            <ProductList
+                products={products}
+                categories={categories}
+                suppliers={suppliers}
+                editingProduct={editingProduct}
+                handleEdit={handleEdit}
+                handleInputChange={handleInputChange}
+                handleUpdate={handleUpdate}
+                handleDelete={handleDelete}
+                handleCheckProduct={checkProductAvailability}
+                handleCategoryAvailability={checkAvailability}
+            />
+            <Button
+                name="Add Product"
+                className="btn btn-primary"
+                onClick={handleAddProductClick}
+            />
+        </div>
+    )
+}
+
+export default ProductPage
