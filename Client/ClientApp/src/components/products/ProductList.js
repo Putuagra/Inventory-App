eb954d5bcad76f275ad2ab5a878fec41f31e1d03
putuagra <iputuagra@gmail.com>
2023-11-22 13:20:48 +0700

    refactor: change product from table to crad

--- Client/ClientApp/src/components/products/ProductList.js
+++ Client/ClientApp/src/components/products/ProductList.js
@@ -7,10 +7,14 @@ import ErrorAlert from '../ErrorAlert'
 import DeleteAlert from '../DeleteAlert'
 import { ValidateData, ValidationDuplicate } from '../../Validation/Products/ProductValidation'
 
+const cardStyle = {
+    maxWidth: '20rem',
+}
 export default function ProductList({ products, categories, suppliers, editingProduct, handleEdit, handleInputChange, handleUpdate, handleDelete, handleCheckProduct, handleCategoryAvailability }) {
 
     const [name, setName] = useState('')
     const [supplier, setSupplier] = useState('')
+    const [category, setCategory] = useState('')
 
     const handleNameEdit = (guid) => {
         const productToEdit = products.find((product) => product.guid === guid)
@@ -20,6 +24,11 @@ export default function ProductList({ products, categories, suppliers, editingPr
     const handleSupplierEdit = (guid) => {
         const productToEdit = products.find((product) => product.guid === guid)
         setSupplier(productToEdit.supplierGuid)
+    }
+
+    const handleCategoryEdit = (guid) => {
+        const productToEdit = products.find((product) => product.guid === guid)
+        setCategory(productToEdit.categoryGuid)
     }
 
     const handleUpdateProduct = async (data) => {
@@ -40,7 +49,7 @@ export default function ProductList({ products, categories, suppliers, editingPr
             return
         }
 
-        if ((status === 404 && statusCategory === 200) || name === data.name) {
+        if ((status === 404 && statusCategory === 200 && name !== data.name) || (name === data.name && supplier === data.supplierGuid && category === data.categoryGuid) || (name === data.name && status === 404 && statusCategory === 200)) {
             try {
                 await handleUpdate(data)
                 SuccessAlert({ message: 'Update product successful.' })
@@ -48,115 +57,110 @@ export default function ProductList({ products, categories, suppliers, editingPr
                 console.error('Error during update:', error)
                 ErrorAlert({ message: 'Failed to update product. Please try again later.' })
             }
-        } 
+        }
+        else if (status === 200 && statusCategory === 200) {
+            ErrorAlert({ message: 'The updated product already exists or is invalid' })
+        }
     }
 
     return (
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>Product Name</th>
-                    <th>Supplier</th>
-                    <th>Category</th>
-                    <th>Stock</th>
-                    <th>Price</th>
-                    <th>Description</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    Array.isArray(products) && products.length > 0 ? (
-                        products.map((data, index) => (
-                            <tr key={index}>
-                                <td>
-                                    {
-                                        editingProduct === data.guid ? (
-                                            <InputUpdate
-                                                name="name"
-                                                type="text"
-                                                value={data.name}
-                                                onChange={(e) => handleInputChange(data.guid, 'name', e.target.value)}
-                                            />
-                                        ) : (
-                                            data.name
-                                        )
-                                    }
-                                </td>
-                                <td>
-                                    {
-                                        editingProduct === data.guid ? (
-                                            <Select
-                                                name="supplierGuid"
-                                                label="Supplier"
-                                                value={data.supplierGuid}
-                                                onChange={(e) => handleInputChange(data.guid, 'supplierGuid', e.target.value)}
-                                                options={suppliers}
-                                            />
-                                        ) : (
-                                            (suppliers.find((supplier) => supplier.guid === data.supplierGuid) || {}).name
-                                        )
-                                    }
-                                </td>
-                                <td>
-                                    {
-                                        editingProduct === data.guid ? (
-                                            <Select
-                                                name="categoryGuid"
-                                                label="Category"
-                                                value={data.categoryGuid}
-                                                onChange={(e) => handleInputChange(data.guid, 'categoryGuid', e.target.value)}
-                                                options={categories.filter((category) => category.supplierGuid === data.supplierGuid)}
-                                            />
-                                        ) : (
-                                            (categories.find((category) => category.guid === data.categoryGuid) || {}).name
-                                        )
-                                    }
-                                </td>
-                                <td>
-                                    {
-                                        editingProduct === data.guid ? (
-                                            <InputUpdate
-                                                name="stock"
-                                                type="number"
-                                                value={data.stock}
-                                                onChange={(e) => handleInputChange(data.guid, 'stock', e.target.value)}
-                                            />
-                                        ) : (
-                                            data.stock
-                                        )
-                                    }
-                                </td>
-                                <td>
-                                    {
-                                        editingProduct === data.guid ? (
-                                            <InputUpdate
-                                                name="price"
-                                                type="number"
-                                                value={data.price}
-                                                onChange={(e) => handleInputChange(data.guid, 'price', e.target.value)}
-                                            />
-                                        ) : (
-                                            data.price
-                                        )
-                                    }
-                                </td>
-                                <td>
-                                    {
-                                        editingProduct === data.guid ? (
-                                            <InputUpdate
-                                                name="description"
-                                                type="text"
-                                                value={data.description}
-                                                onChange={(e) => handleInputChange(data.guid, 'description', e.target.value)}
-                                            />
-                                        ) : (
-                                            data.description
-                                        )
-                                    }
-                                </td>
-                                <td>
-                                    {editingProduct === data.guid ? (
+        <div className="card-container">
+            {
+                Array.isArray(products) && products.length > 0 ? (
+                        products.map((data, index) =>
+                        <div key={index} className="card text-bg-light mb-3" style={cardStyle}>
+                                <div className="card-header">{data.name}</div>
+                                <div className="card-body">
+                                    <h6 className="card-title">
+                                        Name: { }
+                                        {
+                                            editingProduct === data.guid ? (
+                                                <InputUpdate
+                                                    type="text"
+                                                    value={data.name}
+                                                    onChange={(e) => handleInputChange(data.guid, 'name', e.target.value)}
+                                                />
+                                            ) : (
+                                                data.name
+                                            )
+                                        }
+                                </h6>
+                                    <h6 className="card-text">
+                                        Supplier: { }
+                                        {
+                                            editingProduct === data.guid ? (
+                                                <Select
+                                                    name="supplierGuid"
+                                                    value={data.supplierGuid}
+                                                    onChange={(e) => handleInputChange(data.guid, 'supplierGuid', e.target.value)}
+                                                    options={suppliers}
+                                                />
+                                            ) : (
+                                                (suppliers.find((supplier) => supplier.guid === data.supplierGuid) || {}).name
+                                            )
+                                        }
+                                </h6>
+                                <h6 className="card-text">
+                                        Category: { }
+                                        {
+                                             editingProduct === data.guid ? (
+                                                <Select
+                                                    name="categoryGuid"
+                                                    value={data.categoryGuid}
+                                                    onChange={(e) => handleInputChange(data.guid, 'categoryGuid', e.target.value)}
+                                                    options={categories.filter((category) => category.supplierGuid === data.supplierGuid)}
+                                                />
+                                            ) : (
+                                                (categories.find((category) => category.guid === data.categoryGuid) || {}).name
+                                            )
+                                        }
+                                </h6>
+                                <h6 className="card-text">
+                                        Stock: { }
+                                        {
+                                            editingProduct === data.guid ? (
+                                                <InputUpdate
+                                                    name="stock"
+                                                    type="number"
+                                                    value={data.stock}
+                                                    onChange={(e) => handleInputChange(data.guid, 'stock', e.target.value)}
+                                                />
+                                            ) : (
+                                                data.stock
+                                            )
+                                        }
+                                </h6>
+                                <h6 className="card-text">
+                                        Price: { }
+                                        {
+                                            editingProduct === data.guid ? (
+                                                <InputUpdate
+                                                    name="price"
+                                                    type="number"
+                                                    value={data.price}
+                                                    onChange={(e) => handleInputChange(data.guid, 'price', e.target.value)}
+                                                />
+                                            ) : (
+                                                data.price
+                                            )
+                                        }
+                                </h6>
+                                <h6 className="card-text">
+                                        Description: { }
+                                        {
+                                            editingProduct === data.guid ? (
+                                                <InputUpdate
+                                                    name="description"
+                                                    type="text"
+                                                    value={data.description}
+                                                    onChange={(e) => handleInputChange(data.guid, 'description', e.target.value)}
+                                                />
+                                            ) : (
+                                                data.description
+                                            )
+                                        }
+                                </h6>
+                                {editingProduct === data.guid ? (
                                         <Button
                                             name="Save" 
                                             className="btn btn-success"
@@ -170,6 +174,7 @@ export default function ProductList({ products, categories, suppliers, editingPr
                                                     onClick={() => {
                                                         handleNameEdit(data.guid)
                                                         handleSupplierEdit(data.guid)
+                                                        handleCategoryEdit(data.guid)
                                                         handleEdit(data.guid)
                                                     }}
                                                 />
@@ -180,17 +185,13 @@ export default function ProductList({ products, categories, suppliers, editingPr
                                                 />
                                         </>
                                     )}
-                                </td>
-                            </tr>
-                        )
-                        )
-                    ) : (
-                        <tr>
-                            <td colSpan="3">No product available.</td>
-                        </tr>
+                            </div>
+                        </div>
                     )
-                }
-            </tbody>
-        </table>
+                ) : (
+                    <h5>No product available.</h5>
+                )
+            }     
+        </div>
     )
 }
