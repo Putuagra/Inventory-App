124c11bf1df44a46413406b5735063f0e47ac347
putuagra <iputuagra@gmail.com>
2023-12-06 13:42:18 +0700

    feat: add user role dto get

---
+++ API/DataTransferObjects/UserRoles/UserRoleDtoGet.cs
@@ -0,0 +1,30 @@
+﻿using API.Models;
+
+namespace API.DataTransferObjects.UserRoles;
+
+public class UserRoleDtoGet
+{
+    public Guid Guid { get; set; }
+    public Guid UserGuid { get; set; }
+    public Guid RoleGuid { get; set; }
+
+    public static implicit operator UserRole(UserRoleDtoGet userRoleDtoGet)
+    {
+        return new()
+        {
+            Guid = userRoleDtoGet.Guid,
+            UserGuid = userRoleDtoGet.UserGuid,
+            RoleGuid = userRoleDtoGet.RoleGuid
+        };
+    }
+
+    public static explicit operator UserRoleDtoGet(UserRole userRole)
+    {
+        return new()
+        {
+            Guid = userRole.Guid,
+            UserGuid = userRole.UserGuid,
+            RoleGuid = userRole.RoleGuid
+        };
+    }
+}
