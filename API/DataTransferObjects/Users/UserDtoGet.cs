0d309d1ea16600bd1acae7fe1e7040fa4ae1511d
putuagra <iputuagra@gmail.com>
2023-12-06 13:45:00 +0700

    feat: add otp, is used, expired time dto

--- API/DataTransferObjects/Users/UserDtoGet.cs
+++ API/DataTransferObjects/Users/UserDtoGet.cs
@@ -8,6 +8,9 @@ public class UserDtoGet
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
+    public int? Otp { get; set; }
+    public bool IsUsed { get; set; }
+    public DateTime? ExpiredTime { get; set; }
 
     public static implicit operator User(UserDtoGet userDtoGet)
     {
@@ -17,6 +20,9 @@ public class UserDtoGet
             Name = userDtoGet.Name,
             Email = userDtoGet.Email,
             Password = userDtoGet.Password,
+            Otp = userDtoGet.Otp,
+            IsUsed = userDtoGet.IsUsed,
+            ExpiredTime = userDtoGet.ExpiredTime,
         };
     }
 
@@ -28,6 +34,9 @@ public class UserDtoGet
             Name = user.Name,
             Email = user.Email,
             Password = user.Password,
+            Otp = user.Otp,
+            IsUsed = user.IsUsed,
+            ExpiredTime = user.ExpiredTime,
         };
     }
 }
