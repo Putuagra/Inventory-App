8d1a1adcb35298c2f95db348307913b440c4a477
putuagra <iputuagra@gmail.com>
2023-12-06 13:41:25 +0700

    feat: add role dto create

---
+++ API/DataTransferObjects/Roles/RoleDtoCreate.cs
@@ -0,0 +1,24 @@
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Roles;
+
+public class RoleDtoCreate
+{
+    public string Name { get; set; }
+
+    public static implicit operator Role(RoleDtoCreate roleDtoCreate)
+    {
+        return new()
+        {
+            Name = roleDtoCreate.Name
+        };
+    }
+
+    public static explicit operator RoleDtoCreate(Role role)
+    {
+        return new()
+        {
+            Name = role.Name,
+        };
+    }
+}
