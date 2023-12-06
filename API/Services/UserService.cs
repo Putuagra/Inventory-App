41b62a09b4bc0f9946bbd5b3b1b693fd636c2c40
putuagra <iputuagra@gmail.com>
2023-12-06 13:50:03 +0700

    feat: add forgot password and change password in user service

--- API/Services/UserService.cs
+++ API/Services/UserService.cs
@@ -5,6 +5,7 @@ using API.Models;
 using API.Utilities.Handlers;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
+using static System.Net.WebRequestMethods;
 
 namespace API.Services;
 
@@ -13,12 +14,14 @@ public class UserService
     private readonly InventoryDbContext _inventoryDbContext;
     private readonly IUserRepository _userRepository;
     private readonly ITokenHandler _tokenHandler;
+    private readonly IEmailHandler _emailHandler;
 
-    public UserService(IUserRepository userRepository, InventoryDbContext inventoryDbContext, ITokenHandler tokenHandler)
+    public UserService(IUserRepository userRepository, InventoryDbContext inventoryDbContext, ITokenHandler tokenHandler, IEmailHandler emailHandler)
     {
         _userRepository = userRepository;
         _inventoryDbContext = inventoryDbContext;
         _tokenHandler = tokenHandler;
+        _emailHandler = emailHandler;
     }
 
     public IEnumerable<UserDtoGet> Get()
@@ -109,5 +112,47 @@ public class UserService
         {
             return "-2";
         }
+    }
+
+    public int ForgotPassword(UserDtoForgotPassword userDtoForgotPassword)
+    {
+        var user = _userRepository.GetUserByEmail(userDtoForgotPassword.Email);
+        if (user is null) return 0;
+        var user_guid = _userRepository.GetByGuid(user.Guid);
+        var otp = new Random().Next(111111, 999999);
+        var isUpdated = _userRepository.Update(new User
+        {
+            Guid = user_guid.Guid,
+            Name = user_guid.Name,
+            Email = user_guid.Email,
+            Password = user_guid.Password,
+            Otp = otp,
+            ExpiredTime = DateTime.Now.AddMinutes(5),
+            IsUsed = false
+        });
+        if (!isUpdated) return -1;
+        _emailHandler.SendEmail(userDtoForgotPassword.Email,"Forgot Password",$"Your OTP is {otp}");
+        return 1;
+    }
+
+    public int ChangePassword(UserDtoChangePassword userDtoChangePassword)
+    {
+        var user = _userRepository.GetUserByEmail(userDtoChangePassword.Email);
+        if (user is null) return 0;
+        var user_guid = _userRepository.GetByGuid(user.Guid);
+        if(user_guid.IsUsed) return -1;
+        if(user_guid.Otp != userDtoChangePassword.Otp) return -2;
+        if(user_guid.ExpiredTime < DateTime.Now) return -3;
+        var isUpdated = _userRepository.Update(new User
+        {
+            Guid = user_guid.Guid,
+            Name = user_guid.Name,
+            Email = user_guid.Email,
+            Password = HashingHandler.Hash(userDtoChangePassword.NewPassword),
+            Otp = user_guid.Otp,
+            ExpiredTime = user_guid.ExpiredTime,
+            IsUsed = true,
+        });
+        return isUpdated ? 1 : -4;
     }
 }
