fb642f175f050fa69f70e9c568fd4a7171fd0256
putuagra <iputuagra@gmail.com>
2023-12-06 13:42:39 +0700

    feat: add user dto change password

---
+++ API/DataTransferObjects/Users/UserDtoChangePassword.cs
@@ -0,0 +1,33 @@
+﻿using API.Models;
+using API.Utilities.Handlers;
+
+namespace API.DataTransferObjects.Users;
+
+public class UserDtoChangePassword
+{
+    public string Email { get; set; }
+    public int Otp { get; set; }
+    public string NewPassword { get; set; }
+    public string ConfirmNewPassword { get; set; }
+
+    /*public static implicit operator User(UserDtoChangePassword userDtoChangePassword)
+    {
+        return new()
+        {
+            Email = userDtoChangePassword.Email,
+            Otp = userDtoChangePassword.Otp,
+            Password = HashingHandler.Hash(userDtoChangePassword.NewPassword),
+            IsUsed = true,
+        };
+    }
+
+    public static explicit operator UserDtoChangePassword(User user)
+    {
+        return new()
+        {
+            Email = user.Email,
+            Otp = (int)user.Otp,
+            NewPassword = HashingHandler.Hash(user.Password)
+        };
+    }*/
+}
