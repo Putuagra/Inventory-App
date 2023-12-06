cd0727ad7a007ae286de5979ca208e26a7734569
putuagra <iputuagra@gmail.com>
2023-12-06 13:49:43 +0700

    feat: create user role service

---
+++ API/Services/UserRoleService.cs
@@ -0,0 +1,57 @@
+﻿using API.Contracts;
+using API.DataTransferObjects.UserRoles;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Services;
+
+public class UserRoleService
+{
+    private readonly IUserRoleRepository _userRoleRepository;
+
+    public UserRoleService(IUserRoleRepository userRoleRepository)
+    {
+        _userRoleRepository = userRoleRepository;
+    }
+
+    public IEnumerable<UserRoleDtoGet> Get()
+    {
+        var userRoles = _userRoleRepository.GetAll().ToList();
+        if(!userRoles.Any()) return Enumerable.Empty<UserRoleDtoGet>();
+        List<UserRoleDtoGet> userRoleDtoGets = new List<UserRoleDtoGet>();
+        foreach(var userRole in userRoles)
+        {
+            userRoleDtoGets.Add((UserRoleDtoGet)userRole);
+        }
+        return userRoleDtoGets;
+    }
+
+    public UserRoleDtoGet? Get(Guid guid)
+    {
+        var userRole = _userRoleRepository.GetByGuid(guid);
+        if(userRole is null) return null;
+        return (UserRoleDtoGet)userRole;
+    }
+
+    public UserRoleDtoCreate? Create(UserRoleDtoCreate userRoleDtoCreate)
+    {
+        var userRoleCreated = _userRoleRepository.Create(userRoleDtoCreate);
+        if( userRoleCreated is null ) return null;
+        return((UserRoleDtoCreate)userRoleCreated);
+    }
+
+    public int Update(UserRoleDtoUpdate userRoleDtoUpdate)
+    {
+        var userRole = _userRoleRepository.GetByGuid(userRoleDtoUpdate.Guid);
+        if( userRole is null ) return -1;
+        var userRoleUpdated = _userRoleRepository.Update(userRoleDtoUpdate);
+        return userRoleUpdated ? 1 : 0;
+    }
+
+    public int Delete(Guid guid)
+    {
+        var userRole = _userRoleRepository.GetByGuid(guid);
+        if(userRole is null) return -1;
+        var userRoleDeleted = _userRoleRepository.Delete(userRole);
+        return userRoleDeleted ? 1 : 0;
+    }
+}
