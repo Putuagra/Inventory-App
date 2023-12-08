8cb0f50b13cd9305a536443554ec42ec2006e8d8
putuagra <iputuagra@gmail.com>
2023-12-08 13:44:45 +0700

    feat: create change password page in client

---
+++ Client/ClientApp/src/pages/ChangePassword.jsx
@@ -0,0 +1,117 @@
+import { useState } from "react"
+import Button from "../components/Button"
+import Input from "../components/Input"
+import AuthRepository from "../repositories/AuthRepository"
+import ErrorAlert from "../components/ErrorAlert"
+import SuccessAlert from "../components/SuccessAlert"
+import { useNavigate } from 'react-router-dom'
+
+const ChangePassword = () => {
+
+    const { handleChangePassword } = AuthRepository()
+
+    const [changePasswordData, setChangePasswordData] = useState({
+        email: '',
+        otp: '',
+        newPassword: '',
+        confirmNewPassword: ''
+    })
+
+    const [errors, setErrors] = useState({})
+
+    const navigateLogin = useNavigate()
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setChangePasswordData({ ...changePasswordData, [name]: value })
+    }
+
+    const handleChangeNewPassword = async () => {
+        try {
+            const response = await handleChangePassword(changePasswordData)
+            switch (response) {
+                case 404:
+                    ErrorAlert({ message: 'Email not found' })
+                    break
+                case 400:
+                    ErrorAlert({ message: 'Otp is expired or is incorrect' })
+                    break
+                case 500:
+                    ErrorAlert({ message: 'Error retrieving data from the database' })
+                    break
+                case 200:
+                    if (changePasswordData.newPassword === changePasswordData.confirmNewPassword) {
+                        SuccessAlert({ message: 'Password has been changed successfully.' })
+                        navigateLogin("/login")
+                    } else {
+                    ErrorAlert({ message: 'New Password and Confirm New Password do not match.' })
+                    }    
+                    break
+                default:
+                    ErrorAlert({ message: 'Failed to change password. Please try again later.' })
+                    break
+            }
+        } catch (error) {
+            ErrorAlert({ message: 'An unexpected error occurred.' })
+        }
+    }
+
+    const handleValidation = (e) => {
+        e.preventDefault()
+        /*setErrors(Validation(newUser))*/
+        handleChangeNewPassword()
+    }
+
+    return (
+        <div className="container-register">
+            <div className="register-form" noValidate>
+                <h1>Change Password</h1>
+                <form
+                    onSubmit={handleValidation}
+                    className="row g-3 needs-validation"
+                >
+                    <Input
+                        name="email"
+                        type="text"
+                        placeholder="Email"
+                        value={changePasswordData.email}
+                        onChange={handleChange}
+                    />
+                    <Input
+                        name="otp"
+                        type="number"
+                        placeholder="OTP"
+                        value={changePasswordData.otp}
+                        onChange={handleChange}
+                    />
+                    <Input
+                        name="newPassword"
+                        type="password"
+                        placeholder="New Password"
+                        value={changePasswordData.newPassword}
+                        onChange={handleChange}
+                    />
+                    <Input
+                        name="confirmNewPassword"
+                        type="password"
+                        placeholder="Confirm New Password"
+                        value={changePasswordData.confirmNewPassword}
+                        onChange={handleChange}
+                    />
+                    <Button
+                        name="Change Password"
+                        className="btn btn-primary"
+                    />
+                </form>
+                {/*<br></br>
+                <Button
+                    name="Login"
+                    className="btn btn-success"
+                    onClick={handleLoginClick}
+                />*/}
+            </div>
+        </div>
+    )
+}
+
+export default ChangePassword
