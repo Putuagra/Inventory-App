60db613ceefe49208e3d09c80326c5341c889843
putuagra <iputuagra@gmail.com>
2023-12-12 10:40:36 +0700

    feat: add get role name in role repository

--- API/Repositories/RoleRepository.cs
+++ API/Repositories/RoleRepository.cs
@@ -9,4 +9,9 @@ public class RoleRepository : GeneralRepository<Role>, IRoleRepository
     public RoleRepository(InventoryDbContext context) : base(context)
     {
     }
+
+    public Role? GetByName(string name)
+    {
+        return Context.Set<Role>().FirstOrDefault(r => r.Name.ToLower() == name.ToLower());
+    }
 }
