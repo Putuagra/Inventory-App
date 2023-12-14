63c8c7fcf70af6e27627f1c60de4a77c4ec94a74
putuagra <iputuagra@gmail.com>
2023-12-14 13:35:57 +0700

    feat: add back button

--- Client/ClientApp/src/pages/AddUserRole.jsx
+++ Client/ClientApp/src/pages/AddUserRole.jsx
@@ -2,19 +2,29 @@ import { useNavigate } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import UserRoleForm from '../components/userRoles/UserRoleForm'
 import UserRoleRepository from '../repositories/UserRoleRepository'
+import Button from '../components/Button'
 
 const AddUserRole = () => {
     const navigate = useNavigate()
-
-    const { handleCreate } = UserRoleRepository()
+ 
     const location = useLocation()
     const guid = location.state?.guid
+    const { handleCreate } = UserRoleRepository()
+
+    const handleBackClick = () => {
+        navigate("/user-role", { state: { guid } })
+    }
 
     return (
         <div>
             <UserRoleForm
                 handleCreate={handleCreate}
-                roleGuid={guid}
+                roleGuid={guid }
+            />
+            <Button
+                name="Back"
+                className="btn btn-primary"
+                onClick={handleBackClick}
             />
         </div>
     )
