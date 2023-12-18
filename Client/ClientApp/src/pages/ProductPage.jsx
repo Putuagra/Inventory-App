9d3408088c63e78f899373baf0e113454a0bbd83
putuagra <iputuagra@gmail.com>
2023-12-18 09:18:37 +0700

    refactor: remove unused props

--- Client/ClientApp/src/pages/ProductPage.jsx
+++ Client/ClientApp/src/pages/ProductPage.jsx
@@ -1,14 +1,12 @@
-import { checkAvailability } from "../apis/CategoryApi"
-import { checkProductAvailability } from "../apis/ProductApi"
 import { useNavigate } from 'react-router-dom'
 import ProductRepository from "../repositories/ProductRepository"
 import Button from "../components/Button"
 import Navigate from "../components/Navigate"
-import ProductList from "../components/products/ProductList"
+import { ProductList } from "../components/products/ProductList"
 
 const ProductPage = () => {
     const navigate = useNavigate()
-    const { products, categories, suppliers, editingProduct, handleEdit, handleInputChange, handleUpdate, handleDelete } = ProductRepository()
+    const { products, categories, suppliers, handleDelete } = ProductRepository()
     const handleAddProductClick = () => {
         navigate("/add-product")
     }
@@ -19,13 +17,7 @@ const ProductPage = () => {
                 products={products}
                 categories={categories}
                 suppliers={suppliers}
-                editingProduct={editingProduct}
-                handleEdit={handleEdit}
-                handleInputChange={handleInputChange}
-                handleUpdate={handleUpdate}
                 handleDelete={handleDelete}
-                handleCheckProduct={checkProductAvailability}
-                handleCategoryAvailability={checkAvailability}
             />
             <Button
                 name="Add Product"
