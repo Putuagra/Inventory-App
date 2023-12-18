d11afc14ef204a7346e44ac8777fc5b5e2307feb
putuagra <iputuagra@gmail.com>
2023-12-18 15:45:30 +0700

    feat: add get transaction by id in client

--- Client/ClientApp/src/apis/TransactionApi.js
+++ Client/ClientApp/src/apis/TransactionApi.js
@@ -78,3 +78,20 @@ export const remove = async (transactionGuid) => {
         }
     }
 }
+
+export const GetTransactionById = async (guid) => {
+    const token = await GetAuth()
+    const headers = {
+        Authorization: `Bearer ${token}`,
+    }
+    try {
+        const response = await axios.get(`${apiUrl}/Transaction/${guid}`, { headers })
+        return response
+    }
+    catch (error) {
+        if (error.response && error.response.status === 404) {
+            return 404
+        }
+        console.error('Error during API request:', error)
+    }
+}
