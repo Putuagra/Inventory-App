d5fce27fee2749930b51ec17d52525dbab91d780
putuagra <iputuagra@gmail.com>
2023-12-18 11:05:01 +0700

    feat: add get supplier by id in client

--- Client/ClientApp/src/apis/SupplierApi.js
+++ Client/ClientApp/src/apis/SupplierApi.js
@@ -111,3 +111,20 @@ export const checkName = async (name) => {
         console.error('Error during API request:', error)
     }
 }
+
+export const GetSupplierById = async (guid) => {
+    const token = await GetAuth()
+    const headers = {
+        Authorization: `Bearer ${token}`,
+    }
+    try {
+        const response = await axios.get(`${apiUrl}/Supplier/${guid}`, { headers })
+        return response
+    }
+    catch (error) {
+        if (error.response && error.response.status === 404) {
+            return 404
+        }
+        console.error('Error during API request:', error)
+    }
+}
