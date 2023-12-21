72a1e23e4ffcb8d342214744ab4de093b5f31d17
putuagra <iputuagra@gmail.com>
2023-12-21 14:56:16 +0700

    feat: add migration

---
+++ API/Migrations/20231221074827_addAccountTable.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class addAccountTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_tr_user_roles");
+
+            migrationBuilder.DropColumn(
+                name: "expired_time",
+                table: "tb_m_users");
+
+            migrationBuilder.DropColumn(
+                name: "is_used",
+                table: "tb_m_users");
+
+            migrationBuilder.DropColumn(
+                name: "otp",
+                table: "tb_m_users");
+
+            migrationBuilder.DropColumn(
+                name: "password",
+                table: "tb_m_users");
+
+            migrationBuilder.CreateTable(
+                name: "tb_m_accounts",
+                columns: table => new
+                {
+                    guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    otp = table.Column<int>(type: "int", nullable: true),
+                    is_used = table.Column<bool>(type: "bit", nullable: false),
+                    expired_time = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    created_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_m_accounts", x => x.guid);
+                    table.ForeignKey(
+                        name: "FK_tb_m_accounts_tb_m_users_guid",
+                        column: x => x.guid,
+                        principalTable: "tb_m_users",
+                        principalColumn: "guid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_tr_account_roles",
+                columns: table => new
+                {
+                    guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    account_guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    role_guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    created_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_tr_account_roles", x => x.guid);
+                    table.ForeignKey(
+                        name: "FK_tb_tr_account_roles_tb_m_accounts_account_guid",
+                        column: x => x.account_guid,
+                        principalTable: "tb_m_accounts",
+                        principalColumn: "guid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tb_tr_account_roles_tb_m_roles_role_guid",
+                        column: x => x.role_guid,
+                        principalTable: "tb_m_roles",
+                        principalColumn: "guid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_tr_account_roles_account_guid",
+                table: "tb_tr_account_roles",
+                column: "account_guid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_tr_account_roles_role_guid",
+                table: "tb_tr_account_roles",
+                column: "role_guid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_tr_account_roles");
+
+            migrationBuilder.DropTable(
+                name: "tb_m_accounts");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "expired_time",
+                table: "tb_m_users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_used",
+                table: "tb_m_users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "otp",
+                table: "tb_m_users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "password",
+                table: "tb_m_users",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "tb_tr_user_roles",
+                columns: table => new
+                {
+                    guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    role_guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    user_guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    created_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_tr_user_roles", x => x.guid);
+                    table.ForeignKey(
+                        name: "FK_tb_tr_user_roles_tb_m_roles_role_guid",
+                        column: x => x.role_guid,
+                        principalTable: "tb_m_roles",
+                        principalColumn: "guid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tb_tr_user_roles_tb_m_users_user_guid",
+                        column: x => x.user_guid,
+                        principalTable: "tb_m_users",
+                        principalColumn: "guid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_tr_user_roles_role_guid",
+                table: "tb_tr_user_roles",
+                column: "role_guid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_tr_user_roles_user_guid",
+                table: "tb_tr_user_roles",
+                column: "user_guid");
+        }
+    }
+}
