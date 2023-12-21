16b48561260102738bf67a3081f378f4c0d9f380
putuagra <iputuagra@gmail.com>
2023-12-21 14:55:41 +0700

    feat: create register dto

---
+++ API/DataTransferObjects/Accounts/AccountDtoRegister.cs
@@ -0,0 +1,31 @@
+﻿namespace API.DataTransferObjects.Accounts;
+
+public class AccountDtoRegister
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+
+    /*public static implicit operator User(AccountDtoRegister userDtoRegister)
+    {
+        return new()
+        {
+            Name = userDtoRegister.Name,
+            Email = userDtoRegister.Email,
+            Password = HashingHandler.Hash(userDtoRegister.Password),
+            Otp = 0,
+            IsUsed = false
+        };
+    }
+
+    public static explicit operator AccountDtoRegister(User user)
+    {
+        return new()
+        {
+            Name = user.Name,
+            Email = user.Email,
+            Password = HashingHandler.Hash(user.Password),
+        };
+    }*/
+}
