755285b6557119e97d756450991b498fdbd32c40
putuagra <iputuagra@gmail.com>
2023-12-21 14:55:06 +0700

    feat: create account crud dto

---
+++ API/DataTransferObjects/Accounts/AccountDtoCreate.cs
@@ -0,0 +1,41 @@
+﻿using API.Models;
+using API.Utilities.Handlers;
+
+namespace API.DataTransferObjects.Accounts;
+
+public class AccountDtoCreate
+{
+    public Guid Guid { get; set; }
+    public string Password { get; set; }
+    public bool IsDeleted { get; set; }
+    public int? Otp { get; set; }
+    public bool IsUsed { get; set; }
+    public DateTime? ExpiredTime { get; set; }
+
+    // implicit operator
+    public static implicit operator Account(AccountDtoCreate accountDtoCreate)
+    {
+        return new Account
+        {
+            Guid = accountDtoCreate.Guid,
+            Password = HashingHandler.Hash(accountDtoCreate.Password),
+            Otp = accountDtoCreate.Otp,
+            IsUsed = accountDtoCreate.IsUsed,
+            ExpiredTime = accountDtoCreate.ExpiredTime,
+            CreatedDate = DateTime.UtcNow
+        };
+    }
+
+    // explicit operator
+    public static explicit operator AccountDtoCreate(Account account)
+    {
+        return new AccountDtoCreate
+        {
+            Guid = account.Guid,
+            Password = account.Password,
+            Otp = account.Otp,
+            IsUsed = account.IsUsed,
+            ExpiredTime = account.ExpiredTime
+        };
+    }
+}
---
+++ API/DataTransferObjects/Accounts/AccountDtoUpdate.cs
@@ -0,0 +1,40 @@
+﻿using API.Models;
+using API.Utilities.Handlers;
+
+namespace API.DataTransferObjects.Accounts;
+
+public class AccountDtoUpdate
+{
+    public Guid Guid { get; set; }
+    public string Password { get; set; }
+    public int? Otp { get; set; }
+    public bool IsUsed { get; set; }
+    public DateTime? ExpiredTime { get; set; }
+
+    // implicit operator
+    public static implicit operator Account(AccountDtoUpdate accountDtoUpdate)
+    {
+        return new Account
+        {
+            Guid = accountDtoUpdate.Guid,
+            Password = HashingHandler.Hash(accountDtoUpdate.Password),
+            Otp = accountDtoUpdate.Otp,
+            IsUsed = accountDtoUpdate.IsUsed,
+            ExpiredTime = accountDtoUpdate.ExpiredTime,
+            ModifiedDate = DateTime.UtcNow
+        };
+    }
+
+    // explicit operator
+    public static explicit operator AccountDtoUpdate(Account account)
+    {
+        return new AccountDtoUpdate
+        {
+            Guid = account.Guid,
+            Password = account.Password,
+            Otp = account.Otp,
+            IsUsed = account.IsUsed,
+            ExpiredTime = account.ExpiredTime
+        };
+    }
+}
