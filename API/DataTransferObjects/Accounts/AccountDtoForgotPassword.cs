638013ca0f0819e231a25dd16442d419b6868eef
putuagra <iputuagra@gmail.com>
2023-12-21 14:55:16 +0700

    feat: create forgot password dto

---
+++ API/DataTransferObjects/Accounts/AccountDtoForgotPassword.cs
@@ -0,0 +1,26 @@
+﻿namespace API.DataTransferObjects.Accounts;
+
+public class AccountDtoForgotPassword
+{
+    public string Email { get; set; }
+
+    /*public static implicit operator User(UserDtoForgotPassword userDtoForgotPassword)
+    {
+        return new()
+        {
+            Guid = userDtoForgotPassword.Guid,
+            Email = userDtoForgotPassword.Email,
+            Otp = new Random().Next(111111, 999999),
+            ExpiredTime = DateTime.Now.AddMinutes(5),
+            IsUsed = false,
+        };
+    }
+
+    public static explicit operator UserDtoForgotPassword(User user)
+    {
+        return new()
+        {
+            Email = user.Email,
+        };
+    }*/
+}
