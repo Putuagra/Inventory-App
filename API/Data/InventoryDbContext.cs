458c538176fc01560d278d4f1ae0c60bcb6578da
putuagra <iputuagra@gmail.com>
2023-12-21 14:53:23 +0700

    feat: add account and account role table

--- API/Data/InventoryDbContext.cs
+++ API/Data/InventoryDbContext.cs
@@ -7,16 +7,15 @@ public class InventoryDbContext : DbContext
 {
     public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options) { }
 
-
-
     // Tables
+    public DbSet<Account> Accounts { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Supplier> Suppliers { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
     public DbSet<Role> Roles { get; set; }
-    public DbSet<UserRole> USerRoles { get; set; }
+    public DbSet<AccountRole> AccountRoles { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -44,17 +43,23 @@ public class InventoryDbContext : DbContext
             .WithOne(transaction => transaction.User)
             .HasForeignKey(transaction => transaction.UserGuid);
 
-        // User -> UserRole
-        modelBuilder.Entity<User>()
-            .HasMany(user => user.UserRoles)
-            .WithOne(userRole => userRole.User)
-            .HasForeignKey(UserRole => UserRole.UserGuid);
+        // Account -> AccountRole
+        modelBuilder.Entity<Account>()
+            .HasMany(account => account.AccountRoles)
+            .WithOne(accountRole => accountRole.Account)
+            .HasForeignKey(accountRole => accountRole.AccountGuid);
+
+        // Account - User
+        modelBuilder.Entity<Account>()
+            .HasOne(account => account.User)
+            .WithOne(user => user.Account)
+            .HasForeignKey<Account>(account => account.Guid);
 
         // Role -> UserRole
         modelBuilder.Entity<Role>()
-            .HasMany(role => role.UserRoles)
-            .WithOne(userRole => userRole.Role)
-            .HasForeignKey(UserRole => UserRole.RoleGuid);
+            .HasMany(role => role.AccountRoles)
+            .WithOne(accountRole => accountRole.Role)
+            .HasForeignKey(accountRole => accountRole.RoleGuid);
 
         // Supplier - Product
         modelBuilder.Entity<Supplier>()
