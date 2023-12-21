ffd8fafc3bb2feca9569c67660c15780d688f448
putuagra <iputuagra@gmail.com>
2023-12-21 14:50:14 +0700

    feat: create account role contract

---
+++ API/Contracts/IAccountRoleRepository.cs
@@ -0,0 +1,9 @@
+﻿using API.Models;
+
+namespace API.Contracts;
+
+public interface IAccountRoleRepository : IGeneralRepository<AccountRole>
+{
+    AccountRole? CheckAccountRole(Guid accountGuid, Guid roleGuid);
+    IEnumerable<AccountRole> GetAccountRolesByAccountGuid(Guid guid);
+}
