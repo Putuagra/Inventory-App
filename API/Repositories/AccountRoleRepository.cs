e024522b97dd00f363f237a15c41758b27c051b9
putuagra <iputuagra@gmail.com>
2023-12-21 14:57:36 +0700

    feat: create account role repository

---
+++ API/Repositories/AccountRoleRepository.cs
@@ -0,0 +1,22 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class AccountRoleRepository : GeneralRepository<AccountRole>, IAccountRoleRepository
+{
+    public AccountRoleRepository(InventoryDbContext context) : base(context)
+    {
+    }
+
+    public AccountRole? CheckAccountRole(Guid accountGuid, Guid roleGuid)
+    {
+        return Context.Set<AccountRole>().FirstOrDefault(x => x.AccountGuid == accountGuid && x.RoleGuid == roleGuid);
+    }
+
+    public IEnumerable<AccountRole> GetAccountRolesByAccountGuid(Guid guid)
+    {
+        return Context.Set<AccountRole>().Where(accountRole => accountRole.AccountGuid == guid);
+    }
+}
