a7307e69658a146d31df0756979534f670b109b9
putuagra <iputuagra@gmail.com>
2023-12-22 10:14:41 +0700

    refactor: remove unused

--- Client/ClientApp/src/components/users/UserList.js
+++ Client/ClientApp/src/components/users/UserList.js
@@ -1,45 +1,23 @@
 import React, { useState } from 'react'
-import InputUpdate from '../InputUpdate'
 import Button from '../Button'
-import SuccessAlert from '../SuccessAlert'
-import ErrorAlert from '../ErrorAlert'
 import DeleteAlert from '../DeleteAlert'
-import { ValidateData, ValidationDuplicate } from '../../Validation/Users/UserValidation'
+import { useNavigate } from 'react-router-dom'
 
-export default function UserList({ users, editingUser, handleEdit, handleInputChange, handleUpdate, handleDelete, handleCheckEmail }) {
+export default function UserList({ users, handleDelete }) {
 
-    const [email, setEmail] = useState('')
+    const navigate = useNavigate()
 
-    const handleEmailEdit = (guid) => {
-        const userToEdit = users.find((user) => user.guid === guid);
-        setEmail(userToEdit.email)
-    }
-
-    const handleUpdateUser = async (data) => {
-        const validationError = ValidateData(data)
-
-        if (validationError) {
-            ErrorAlert({ message: validationError })
-            return
-        }
-
-        const emailStatus = await handleCheckEmail(data.email)
-        const validateDuplicate = ValidationDuplicate(data, emailStatus, email)
-        if (validateDuplicate) {
-            ErrorAlert({ message: validateDuplicate })
-            return
-        } else if (emailStatus === 404 || email === data.email) { 
-            try {
-                await handleUpdate(data)
-                SuccessAlert({ message: 'Update data successful.' })
-            } catch (error) {
-                console.error('Error during update data:', error);
-                ErrorAlert({ message: 'Failed to update user. Please try again later.' })
+    const handleUpdateClick = (guid) => {
+        const userToEdit = users.find((user) => user.guid === guid)
+        const prevUserEmail = userToEdit.email
+        navigate("/update-user", {
+            state: {
+                guid,
+                prevUserEmail: prevUserEmail
             }
-        } else {
-            ErrorAlert({ message: 'Failed to check email availability. Please try again later.' })
-        }  
+        })
     }
+
     return (
         <div>
             <table className="table table-striped">
@@ -55,53 +33,24 @@ export default function UserList({ users, editingUser, handleEdit, handleInputCh
                         users.map((data, index) => (
                             <tr key={index}>
                                 <td>
-                                    {editingUser === data.guid ? (
-                                        <InputUpdate
-                                            name="name"
-                                            type="text"
-                                            value={data.name}
-                                            onChange={(e) => handleInputChange(data.guid, 'name', e.target.value)}
-                                        />
-                                    ) : (
-                                        data.name
-                                    )}
+                                    {data.name}
                                 </td>
                                 <td>{
-                                    editingUser === data.guid ? (
-                                        <InputUpdate
-                                            name="email"
-                                            type="text"
-                                            value={data.email}
-                                            onChange={(e) => handleInputChange(data.guid, 'email', e.target.value)}
-                                        />
-                                    ) : (
-                                        data.email
-                                    )}
+                                    data.email}
                                 </td>
                                 <td>
-                                    {editingUser === data.guid ? (
-                                        <Button
-                                            name="Save"
-                                            className="btn btn-success"
-                                            onClick={() => handleUpdateUser(data)}
-                                        />
-                                    ) : (
-                                        <>
-                                            <Button
-                                                name="Edit"
-                                                className="btn btn-primary"
-                                                onClick={() => {
-                                                    handleEmailEdit(data.guid)
-                                                    handleEdit(data.guid)
-                                                }}
-                                            />
-                                            <Button
-                                                name="Delete"
-                                                className="btn btn-danger"
-                                                onClick={() => DeleteAlert({ handleDelete, guid: data.guid })}
-                                            />
-                                        </>
-                                    )}
+                                    <Button
+                                        name="Edit"
+                                        className="btn btn-primary"
+                                        onClick={() => {
+                                            handleUpdateClick(data.guid)
+                                        }}
+                                    />
+                                    <Button
+                                        name="Delete"
+                                        className="btn btn-danger"
+                                        onClick={() => DeleteAlert({ handleDelete, guid: data.guid })}
+                                    />
                                 </td>
                             </tr>
                         ))
