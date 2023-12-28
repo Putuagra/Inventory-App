131847bb0005e3e44031c6e62d1435bc28de576d
putuagra <iputuagra@gmail.com>
2023-12-28 13:39:17 +0700

    refactor: remove logout button in user page

--- Client/ClientApp/src/pages/UserPage.jsx
+++ Client/ClientApp/src/pages/UserPage.jsx
@@ -1,32 +1,17 @@
-import { RemoveAuth } from "../components/Auth"
-import Button from "../components/Button"
-import { useNavigate } from 'react-router-dom'
 import Navigate from "../components/Navigate"
 import UserList from "../components/users/UserList"
 import UserRepository from "../repositories/UserRepository"
 
 const UserPage = () => {
-    const { users, handleDelete, nameDecode } = UserRepository()
-    const navigateLogout = useNavigate()
-
-    const handleLogout = () => {
-        RemoveAuth()
-        navigateLogout('/login')
-    }
+    const { users, handleDelete } = UserRepository()
 
     return (
         <div className="container">
             <Navigate />
             <br></br>
-            <h1>Welcome, {nameDecode}</h1>
             <UserList
                 users={users}
                 handleDelete={handleDelete}
-            />
-            <Button
-                name="Logout"
-                className="btn btn-danger"
-                onClick={handleLogout}
             />
         </div>
     )
