57413f0bb1d1c8f19aa75df2ec89e64e3f5f994c
putuagra <iputuagra@gmail.com>
2023-12-28 15:13:46 +0700

    feat: add show transaction for who logged in except admin

--- Client/ClientApp/src/components/transactions/TransactionList.js
+++ Client/ClientApp/src/components/transactions/TransactionList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '../Button'
 import DeleteAlert from '../DeleteAlert'
 import { useNavigate } from 'react-router-dom'
+import { GetAuth, GetTokenClaim } from '../Auth'
 
 const cardStyle = {
     maxWidth: '20rem',
@@ -12,6 +13,11 @@ export default function TransactionList(props) {
     const { products, users, transactions, handleDelete } = props
 
     const navigate = useNavigate()
+    const token = GetAuth()
+    const decode = GetTokenClaim(token)
+    const rolesClaim = decode["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
+
+    const filteredTransactions = transactions.filter(transaction => transaction.userGuid === decode.Guid)
 
     const handleUpdateClick = (guid) => {
         const transactionToEdit = transactions.find((transaction) => transaction.guid === guid)
@@ -31,6 +37,7 @@ export default function TransactionList(props) {
             <br></br>
             {
                 Array.isArray(transactions) && transactions.length > 0 ? (
+                    rolesClaim.includes("Admin") ? (
                     transactions.map((data, index) =>
                         <div key={index} className="card text-bg-light mb-3" style={cardStyle}>
                             <div className="card-header">{(products.find((product) => product.guid === data.productGuid) || {}).name}</div>
@@ -57,18 +64,46 @@ export default function TransactionList(props) {
                                         (products.find((product) => product.guid === data.productGuid) || {}).price * data.quantity
                                     }
                                 </h6>
-                                <Button
-                                    name="Edit"
-                                    className="btn btn-primary"
-                                    onClick={() => {
-                                        handleUpdateClick(data.guid)
-                                    }}
-                                />
-                                <Button
-                                    name="Delete"
-                                    className="btn btn-danger"
-                                    onClick={() => DeleteAlert({ handleDelete, guid: data.guid })}
-                                />
+                                    <Button
+                                        name="Edit"
+                                        className="btn btn-primary"
+                                        onClick={() => {
+                                            handleUpdateClick(data.guid)}}
+                                    />
+                                    <Button
+                                        name="Delete"
+                                        className="btn btn-danger"
+                                        onClick={() => DeleteAlert({ handleDelete, guid: data.guid })}
+                                    />
+                            </div>
+                        </div>
+                        )
+                    ) : filteredTransactions.map((data, index) =>
+                        <div key={index} className="card text-bg-light mb-3" style={cardStyle}>
+                            <div className="card-header">{(products.find((product) => product.guid === data.productGuid) || {}).name}</div>
+                            <div className="card-body">
+                                <h6 className="card-title">
+                                    {
+                                        (products.find((product) => product.guid === data.productGuid) || {}).name
+                                    }
+                                </h6>
+                                <h6 className="card-text">
+                                    {
+                                        (users.find((user) => user.guid === data.userGuid) || {}).name
+                                    }
+                                </h6>
+                                <h6 className="card-text">
+                                    Quantity:
+                                    {
+                                        data.quantity
+                                    }
+                                </h6>
+                                <h6 className="card-text">
+                                    Price:
+                                    {
+                                        (products.find((product) => product.guid === data.productGuid) || {}).price * data.quantity
+                                    }
+                                </h6>
                             </div>
                         </div>
                     )
