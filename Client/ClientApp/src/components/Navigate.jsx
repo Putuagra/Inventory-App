9315799acf8362d5e9b0a3aca584b04c1fa2523d
putuagra <iputuagra@gmail.com>
2023-12-28 14:15:26 +0700

    feat: add roles claim in navigate

--- Client/ClientApp/src/components/Navigate.jsx
+++ Client/ClientApp/src/components/Navigate.jsx
@@ -1,6 +1,11 @@
 import { NavLink } from 'react-router-dom'
+import { GetAuth, GetTokenClaim } from './Auth'
 
 const Navigate = () => {
+    const token = GetAuth()
+    const decode = GetTokenClaim(token)
+    const rolesClaim = decode["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
+
     return (
         <nav>
             <ul>
@@ -9,26 +14,34 @@ const Navigate = () => {
                         isActive ? 'active' : ''
                     }>Products</NavLink>
                 </li>
-                <li>
-                    <NavLink to="/category" className={({ isActive }) =>
-                        isActive ? 'active' : ''
-                    }>Categories</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/supplier" className={({ isActive }) =>
-                        isActive ? 'active' : ''
-                    }>Suppliers</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/user" className={({ isActive }) =>
-                        isActive ? 'active' : ''
-                    }>Users</NavLink>
-                </li>
+                {rolesClaim.includes("Admin") ? (
+                    <li>
+                        <NavLink to="/category" className={({ isActive }) =>
+                            isActive ? 'active' : ''
+                        }>Categories</NavLink>
+                    </li>
+                ) : null}
+                {rolesClaim.includes("Admin") ? (
+                    <li>
+                        <NavLink to="/supplier" className={({ isActive }) =>
+                            isActive ? 'active' : ''
+                        }>Suppliers</NavLink>
+                    </li>
+                ) : null}
+                {rolesClaim.includes("Admin") ? (
+                    <li>
+                        <NavLink to="/user" className={({ isActive }) =>
+                            isActive ? 'active' : ''
+                        }>Users</NavLink>
+                    </li>
+                ) : null}
+                {rolesClaim.includes("Admin") ? (
                 <li>
                     <NavLink to="/role" className={({ isActive }) =>
                         isActive ? 'active' : ''
                     }>Roles</NavLink>
                 </li>
+                ) : null}
                 <li>
                     <NavLink to="/transaction" className={({ isActive }) =>
                         isActive ? 'active' : ''
